39279b1d36d8e20ebe90741a7e33ed40f6146631
KTShow <tracey@kleinadvisor.com>
2026-06-09 20:58:07 -0400

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -13,6 +13,8 @@ export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
     persistSession: true,
     autoRefreshToken: true,
+    detectSessionInUrl: true,
+    flowType: 'implicit',
   }
 });
 
